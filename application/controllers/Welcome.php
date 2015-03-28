471e09e68cc29e0692141784e2ffaf34d9945f5a
3rafanan <3rafanan@gmail.com>
2015-03-28 02:13:59 -0700

    Finished homepage with links
    Signed-off-by: 3rafanan <3rafanan@gmail.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,7 +2,7 @@
 
 /**
  * Our homepage. Show the most recently added quote.
- * 
+ *
  * controllers/Welcome.php
  *
  * ------------------------------------------------------------------------
@@ -11,7 +11,8 @@ class Welcome extends Application {
 
     function __construct()
     {
-	parent::__construct();
+        parent::__construct();
+        $this->load->helper('directory');
     }
 
     //-------------------------------------------------------------
@@ -20,13 +21,30 @@ class Welcome extends Application {
 
     function index()
     {
-	// Build a list of orders
-	
-	// Present the list to choose from
-	$this->data['pagebody'] = 'homepage';
-	$this->render();
+        // Build a list of orders
+        // Get all files in data folder
+        $files = directory_map('./data');
+
+        // filter out orders
+        $orders = array();
+        preg_match_all('/(?<=order).*(?=\.xml*)/', implode("\n", $files), $orders);
+
+        $orderlinks = array();
+        foreach($orders[0] as $order)
+        {
+            $orderlink = array(
+                'filenumber'   => $order,
+            );
+            $orderlinks[] = $orderlink;
+        }
+
+        // Present the list to choose from
+        $this->data['pagebody'] = 'homepage';
+        $this->data['orders'] =  $orderlinks;
+
+        $this->render();
     }
-    
+
     //-------------------------------------------------------------
     //  Show the "receipt" for a specific order
     //-------------------------------------------------------------
@@ -34,11 +52,11 @@ class Welcome extends Application {
     function order($filename)
     {
 	// Build a receipt for the chosen order
-	
+
 	// Present the list to choose from
 	$this->data['pagebody'] = 'justone';
 	$this->render();
     }
-    
+
 
 }
--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,3 +1,9 @@
-<div class="row">
-    Burger Bonanza?
+<h4>Barker Bob's Burger Bar - Orders</h4>
+
+{orders}
+<div>
+    <a href="welcome/order/{filenumber}">order{filenumber}</a>
 </div>
+{/orders}
+
+<h4>Select an order from the list above to see its receipt.</h4>
